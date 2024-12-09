fd7b3c4be172443555dc71a9d147302ddff4ab77
bhalothia <sandeepbhalothiabuilds@gmail.com>
2024-12-09 11:25:34 -0500

    Code cleaned

--- src/main/java/com/sam/exclusion/service/CSVParser.java
+++ src/main/java/com/sam/exclusion/service/CSVParser.java
@@ -32,7 +32,7 @@ public class CSVParser {
 
     public void extractExcelData(File file) throws IOException {
 
-        downloadFile();
+        //downloadFile();
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 
