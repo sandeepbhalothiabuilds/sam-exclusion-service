81757c8fc34961949f994a836f48c681f97f6f83
bhalothia <sandeepbhalothiabuilds@gmail.com>
2024-12-09 10:33:14 -0500

    Code cleaned

--- src/main/java/com/sam/exclusion/controller/FilesController.java
+++ src/main/java/com/sam/exclusion/controller/FilesController.java
@@ -1,7 +1,6 @@
 package com.sam.exclusion.controller;
 
 import com.sam.exclusion.message.ResponseMessage;
-import com.sam.exclusion.model.NycRentControlledPropertiesResponse;
 import com.sam.exclusion.service.FilesStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,9 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @CrossOrigin
